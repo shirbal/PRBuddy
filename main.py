a6e80eb58aaa4a229a4ce5af7749bb0fa6d1361f
shirbal <maor.shiran@gmail.com>
2024-10-03 15:06:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from github import Auth
 from ChangeRephraser import ChangeRephraser
 
 def update_pr_description(repo_name, pr_number, new_description):
-    g = Github(GITHUB_TOKEN)
+    g = Github(TOKEN)
     repo = g.get_repo(repo_name)
     pr = repo.get_pull(pr_number)
     pr.edit(body=new_description)
